1d61921d9abb49ff619fe922f38b1873d419d1ae
Roberto de Frutos Jiménez <robertodfj93@gmail.com>
2026-01-21 13:33:30 +0100

    04 -Creamos los metodos necesarios para obtener de la api y guardar el pokemon con las caracteristicas seleccionadas

--- service/PokemonService.cs
+++ service/PokemonService.cs
@@ -18,22 +18,22 @@ namespace Pokemon.service
         public async Task<PokemonResponseDTO> CreatePokemon(CreatePokemonDTO createPokemonDTO, int userId)
         {
             // Obtener el pokemomn de la API externa
-            // Primero obtengo los daos princiopales del pokemon
-            var apiResponse = await _httpClient.GetAsync($"https://pokeapi.co/api/v2/pokemon/{createPokemonDTO.PokemonApiId}");
-            apiResponse.EnsureSuccessStatusCode();
-            var jsonResponse = await apiResponse.Content.ReadAsStringAsync();
-            var pokemonApi = JsonSerializer.Deserialize<PokemonApiDTO>(jsonResponse, new JsonSerializerOptions
-            {
-                PropertyNameCaseInsensitive = true
-            });
-            // Segundo obtengo la categoria / especie del pokemon
-            var apiResponseSpecies = await _httpClient.GetAsync($"https://pokeapi.co/api/v2/pokemon-species/{createPokemonDTO.PokemonApiId}");
-            apiResponseSpecies.EnsureSuccessStatusCode();
-            var jsonResponseSpecies = await apiResponseSpecies.Content.ReadAsStringAsync();
-            var pokemonSpeciesApi = JsonSerializer.Deserialize<PokemonSpeciesDTO>(jsonResponseSpecies, new JsonSerializerOptions
-            {
-                PropertyNameCaseInsensitive = true
-            });
+                // Primero obtengo los daos princiopales del pokemon
+                var apiResponse = await _httpClient.GetAsync($"https://pokeapi.co/api/v2/pokemon/{createPokemonDTO.PokemonApiId}");
+                apiResponse.EnsureSuccessStatusCode();
+                var jsonResponse = await apiResponse.Content.ReadAsStringAsync();
+                var pokemonApi = JsonSerializer.Deserialize<PokemonApiDTO>(jsonResponse, new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                });
+                // Segundo obtengo la categoria / especie del pokemon
+                var apiResponseSpecies = await _httpClient.GetAsync($"https://pokeapi.co/api/v2/pokemon-species/{createPokemonDTO.PokemonApiId}");
+                apiResponseSpecies.EnsureSuccessStatusCode();
+                var jsonResponseSpecies = await apiResponseSpecies.Content.ReadAsStringAsync();
+                var pokemonSpeciesApi = JsonSerializer.Deserialize<PokemonSpeciesDTO>(jsonResponseSpecies, new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                });
             // Guardar el pokemon en la db
             var newPokemon = new model.Pokemon()
             {
@@ -46,9 +46,8 @@ namespace Pokemon.service
             };
             _context.Pokemons.Add(newPokemon);
             await _context.SaveChangesAsync();
-
+            
             // Hacer el retorno de la respuesta con el DTO
-
             return new PokemonResponseDTO()
             {
                 Id = newPokemon.Id,

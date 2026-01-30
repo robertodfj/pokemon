3855ba7747a54ff3943441fa5635df9781bba0bc
RobertoDFJ <robertodfj93@gmail.com>
2026-01-30 10:50:50 +0100

    42 - Añadimos validaciones en species dto y realizamos las pruebas de login y registro

--- dto/PokemonSpeciesDTO.cs
+++ dto/PokemonSpeciesDTO.cs
@@ -2,17 +2,17 @@ namespace Pokemon.dto
 {
     public class PokemonSpeciesDTO
     {
-        public List<GeneraDTO> Genera { get; set; }
+        public required List<GeneraDTO> Genera { get; set; }
     }
 
     public class GeneraDTO
     {
-        public string Genus { get; set; }
-        public LanguageDTO Language { get; set; }
+        public required string Genus { get; set; }
+        public required LanguageDTO Language { get; set; }
     }
 
     public class LanguageDTO
     {
-        public string Name { get; set; }
+        public required string Name { get; set; }
     }
 }
